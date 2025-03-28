bb75dfac799be841f8b29f931fa0f9f5ab67a682
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-03-28 09:02:55 -0500

    feat: formFields dynamically mapped

--- src/InfoForm.jsx
+++ src/InfoForm.jsx
@@ -4,29 +4,30 @@ import { useState } from "react";
 import "./styles/InfoForm.css";
 
 const Form = (props) => {
-  const { open, setOpen } = useState(false);
+  const [open, setOpen] = useState(false);
+
   return (
-    <button className="info-form">
-      <img src={props.svgType} alt="" />
-      <h3>{props.sectionTitle}</h3>
-      <img src={downArrowSVG} alt="" />
-    </button>
+    <div>
+      <button className="info-form" onClick={() => setOpen(!open)}>
+        <img src={props.svgType} alt="" />
+        <h3>{props.sectionTitle}</h3>
+        <img src={downArrowSVG} alt="" />
+      </button>
+
+      {open && <OpenForm formFields={props.formFields} />}
+    </div>
   );
 };
 
-const OpenForm = () => {
+const OpenForm = ({ formFields }) => {
   return (
     <form>
-      <label></label>
-      <input />
-      <label></label>
-      <input />
-      <label></label>
-      <input />
-      <label></label>
-      <input />
-      <label></label>
-      <input />
+      {formFields.map((field, index) => (
+        <div key={index}>
+          <label htmlFor={field}>{field}</label>
+          <input id={field} name={field.toLowerCase().replace(/\s+/g, "")} />
+        </div>
+      ))}
     </form>
   );
 };
